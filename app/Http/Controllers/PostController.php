eabf1917809a645e5e93e638493d27f936a33ec3
yashi-yashi09 <k.m.ak04129@gmail.com>
2024-10-07 21:38:57 +0900

    課題2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+ use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller{
-    public function index (){
-        return view('posts.index');
-    }
-    
+    public function index () {
+    // postsテーブルからすべてのデータを取得し変数$postsに代入
+    $posts = DB::table('posts')->get();
+
+    // $postsをposts/index.blade.phpファイルに渡す
+    return view('posts.index',Compact('posts'));
+}
+
 }
